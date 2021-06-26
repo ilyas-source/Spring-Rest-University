00662ec9dda4b5e5955c76a047ad15ead193374d
Ilyas Saitov <saitov1981@gmail.com>
2021-06-26 20:20:42 +0500

    save

---
+++ src/test/resources/fill-subjects.sql
@@ -0,0 +1,5 @@
+INSERT INTO subjects (name, description) VALUES
+('Test Economics', 'Base economics'),
+('Test Philosophy', 'Base philosophy'),
+('Test Chemistry', 'Base chemistry'),
+('Test Radiology', 'Explore radiation');
