46e14047f5681607cd71b2e189a93fd662317882
Ilyas Saitov <saitov1981@gmail.com>
2021-09-20 11:09:34 +0300

    classroomsView with modal window

--- src/main/java/ua/com/foxminded/university/controller/ClassroomController.java
+++ src/main/java/ua/com/foxminded/university/controller/ClassroomController.java
@@ -5,7 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ua.com.foxminded.university.model.Classroom;
+import ua.com.foxminded.university.model.Location;
 import ua.com.foxminded.university.service.ClassroomService;
 
 @Controller
@@ -25,5 +29,19 @@ public class ClassroomController {
         logger.debug("Retrieving all classrooms to controller");
         model.addAttribute("classrooms", classroomService.findAll());
         return "classroomsView";
+    }
+
+    @PostMapping
+    public String create(@RequestParam("name") String name, @RequestParam("capacity") String capacity, Model model) {
+        logger.debug("Received from form: name {}, capacity {}",name,capacity);
+
+        Location location=new Location("Test building", 10, 100);
+        Classroom classroom=new Classroom(location, name, Integer.valueOf(capacity));
+
+        classroomService.create(classroom);
+
+        model.addAttribute("classrooms", classroomService.findAll());
+
+        return "ClassroomsView";
     }
 }
