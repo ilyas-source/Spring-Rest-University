f924329c2945c6512ec75e36e9098981f3dc7356
Ilyas Saitov <saitov1981@gmail.com>
2021-11-12 11:09:35 +0300

    dev

--- src/main/java/ua/com/foxminded/university/Main.java
+++ src/main/java/ua/com/foxminded/university/Main.java
@@ -10,28 +10,4 @@ public class Main {
         System.setProperty("server.servlet.context-path", "/university");
         SpringApplication.run(Main.class, args);
     }
-
-//    @Bean
-//    public TomcatServletWebServerFactory tomcatFactory() {
-//        return new TomcatServletWebServerFactory() {
-//            @Override
-//            protected TomcatWebServer getTomcatWebServer(org.apache.catalina.startup.Tomcat tomcat) {
-//                tomcat.enableNaming();
-//                return super.getTomcatWebServer(tomcat);
-//            }
-//
-////            @Override
-////            protected void postProcessContext(Context context) {
-////                ContextResource resource = new ContextResource();
-////                //resource.setProperty("factory", "org.apache.tomcat.jdbc.pool.DataSourceFactory");
-////                resource.setName("jdbc/myDatasourceName");
-////                resource.setType(DataSource.class.getName());
-////                resource.setProperty("driverClassName", "oracle.jdbc.OracleDriver");
-////                resource.setProperty("url", "db_url");
-////                resource.setProperty("username", "db_username");
-////                resource.setProperty("password", "db_password");
-////                context.getNamingResources().addResource(resource);
-////            }
-//        };
-//    }
 }
